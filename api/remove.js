0fca17010a241b73a3013aa259ad285f8b734d31
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-05-14 14:08:22 +1000

    Update remove.js

--- api/remove.js
+++ api/remove.js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
 
     return res.status(200).send("Customer removed from CSV");
   } catch (error) {
-    console.error("GitHub CSV update error:", error);
+    console.error("💥 REMOVE ERROR:", error.message || error);
     return res.status(500).send("GitHub update failed");
   }
 }
